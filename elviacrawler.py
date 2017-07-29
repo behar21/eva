cece6fbc236c5a5465806d028366c32b5b0b543d
behar21 <behar21@gmail.com>
2017-07-29 12:36:01 +0200

    Add files via upload

--- elviacrawler.py
+++ elviacrawler.py
@@ -8,90 +8,77 @@ from bs4 import BeautifulSoup
 import time
 from selenium.webdriver.common.keys import Keys
 
-
+chrome=webdriver.PhantomJS()
+chrome.get("https://tarif.elvia.ch/direct/start.do?calc=mfz&variante=pw&lang=de")
+prit = WebDriverWait(chrome,10)
 try:
-	chrome=webdriver.PhantomJS()
-	chrome.get("https://tarif.elvia.ch/direct/start.do?calc=mfz&variante=pw&lang=de")			
-    	dp1=chrome.find_element_by_xpath("//*[@id='id6731']/option[text()='2016']").click()
+				
+  	dp1=chrome.find_element_by_xpath("//*[@id='id6731']/option[text()='2016']").click()
 	
-	wait = WebDriverWait(chrome, 10)
-    	element = wait.until(EC.element_to_be_clickable((By.ID, '8393')))
+	
+   	element = prit.until(EC.element_to_be_clickable((By.ID, '8393')))
 	dp2=chrome.find_element_by_xpath("//*[@id='8393']/option[text()='BMW']").click()
 
-	wait = WebDriverWait(chrome, 10)
-    	element = wait.until(EC.element_to_be_clickable((By.ID, '8440')))
+	element = prit.until(EC.element_to_be_clickable((By.ID, '8440')))
 	dp3=chrome.find_element_by_xpath("//*[@id='8440']/option[text()='2er Serie F46 BENZIN Gran Tourer']").click()
 	
-	wait = WebDriverWait(chrome, 10)
-	element = wait.until(EC.element_to_be_clickable((By.ID, 'id6815')))
+	element = prit.until(EC.element_to_be_clickable((By.ID, 'id6815')))
 	dp4=chrome.find_element_by_xpath("//*[@id='id6815']/option[text()='216i Gran Tourer']").click()
-    	
-	wait = WebDriverWait(chrome,10)
-	element = wait.until(EC.element_to_be_clickable((By.ID,'1730')))
- 	table_buttons = chrome.find_elements_by_xpath("//*[@id='1730']")
-    	
-	
-	prit = WebDriverWait(chrome,10)
-	try:
-        
-		element = prit.until(EC.element_to_be_clickable((By.ID,'1730')))
-		chrome.find_element_by_xpath("//*[@id='1730']").click()
-		print("1st Page Finished")
+    
+    	element = prit.until(EC.element_to_be_clickable((By.ID,'1730')))
+    	chrome.find_element_by_xpath("//*[@id='1730']").click()
+    	print("1st Page Finished")
 
 		#-------Go to 2nd Page -------------
         
-		element = prit.until(EC.element_to_be_clickable((By.ID,'id8214')))
-		chrome.find_element_by_xpath("//*[@id='id8214']/option[text()='AG']").click()
+    	element = prit.until(EC.element_to_be_clickable((By.ID,'id8214')))
+    	chrome.find_element_by_xpath("//*[@id='id8214']/option[text()='AG']").click()
 	
-		kontrollschild = chrome.find_element_by_xpath("//*[@id='id9108']")
-		kontrollschild.clear()
-		kontrollschild.send_keys("123456")
+   	kontrollschild = chrome.find_element_by_xpath("//*[@id='id9108']")
+    	kontrollschild.clear()
+    	kontrollschild.send_keys("123456")
 		
-		inverkehrestzung = chrome.find_element_by_xpath("//*[@id='id9116']")
-		inverkehrestzung.clear()
-		inverkehrestzung.send_keys("28.07.2017")
+    	inverkehrestzung = chrome.find_element_by_xpath("//*[@id='id9116']")
+    	inverkehrestzung.clear()
+    	inverkehrestzung.send_keys("28.07.2017")
 
-		chrome.find_element_by_xpath("//*[@id='id2640']/option[text()='kein Leasing']").click()
-		print("2nd Page Finished")
+    	chrome.find_element_by_xpath("//*[@id='id2640']/option[text()='kein Leasing']").click()
+    	print("2nd Page Finished")
         
 		#------- Got  to 3rd Page ------------
-		chrome.find_element_by_xpath("//*[@id='1730']").click()
+    	chrome.find_element_by_xpath("//*[@id='1730']").click()
 
 
-		element = prit.until(EC.element_to_be_clickable((By.ID,'id10558')))
-        	fuhrerausweis = chrome.find_element_by_xpath("//*[@id='id10558']")
-        	fuhrerausweis.clear()
-        	fuhrerausweis.send_keys("02.02.2017")
+    	element = prit.until(EC.element_to_be_clickable((By.ID,'id10558')))
+    	fuhrerausweis = chrome.find_element_by_xpath("//*[@id='id10558']")
+    	fuhrerausweis.clear()
+    	fuhrerausweis.send_keys("02.02.2017")
             
-        	plz = chrome.find_element_by_xpath("//*[@id='id1001001']")
-        	plz.clear()
-        	plz.send_keys("4016")
-		chrome.find_element_by_xpath("//*[@id='id10558']").click()
+   	plz = chrome.find_element_by_xpath("//*[@id='id1001001']")
+    	plz.clear()
+    	plz.send_keys("4016")
+    	chrome.find_element_by_xpath("//*[@id='id10558']").click()
 
-		element = prit.until(EC.element_to_be_clickable((By.ID,'id1744')))
-        	jahr = chrome.find_element_by_xpath("//*[@id='id1744']")
-       		jahr.clear()
-		jahr.send_keys("5") 
+    	element = prit.until(EC.element_to_be_clickable((By.ID,'id1744')))
+    	jahr = chrome.find_element_by_xpath("//*[@id='id1744']")
+    	jahr.clear()
+   	jahr.send_keys("5")
                 
-        	birthdate = chrome.find_element_by_xpath("//*[@id='8120']")
-            	birthdate.clear()
-            	birthdate.send_keys("28.01.1993")
-		chrome.find_element_by_xpath("//*[@id='id10558']").click()
-		
-		#element = prit.until(EC.element_to_be_clickable((By.ID,'id3348')))
-		#chrome.find_element_by_xpath("//*[@id='id3348']/option[text()='B (Aufenthalt)']").click()
+    	birthdate = chrome.find_element_by_xpath("//*[@id='8120']")
+    	birthdate.clear()
+    	birthdate.send_keys("28.01.1993")
+    	chrome.find_element_by_xpath("//*[@id='id10558']").click()
 				
-		print("3rd Page Finished!!!")		
-		chrome.find_element_by_xpath("//*[@id='1730']").click()
+    	print("3rd Page Finished!!!")
+    	chrome.find_element_by_xpath("//*[@id='1730']").click()
 		
-		element = prit.until(EC.element_to_be_clickable((By.ID,'1730')))
-		chrome.find_element_by_xpath("//*[@id='1730']").click()
+   	element = prit.until(EC.element_to_be_clickable((By.ID,'1730')))
+    	chrome.find_element_by_xpath("//*[@id='1730']").click()
 
-		element = prit.until(EC.element_to_be_clickable((By.ID,'id100071')))
-		total = chrome.find_element_by_xpath("//*[@id='id100071']")
-		print("Total : "+total.text +" CHF")
-	except TimeoutException:
-		print("Ahh damn it!!!")
+    	element = prit.until(EC.element_to_be_clickable((By.ID,'id100071')))
+    	total = chrome.find_element_by_xpath("//*[@id='id100071']")
+    	print("Total : "+total.text +" CHF")
+
 		
 
 except TimeoutException:
